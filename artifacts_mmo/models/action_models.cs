eb88385e63b242264483e6c0cdba363cc917426f
kevinbrink <kbrink170@gmail.com>
2025-01-19 11:16:19 -0600

    Implement fight

--- artifacts_mmo/models/action_models.cs
+++ artifacts_mmo/models/action_models.cs
@@ -3,19 +3,15 @@ using Newtonsoft.Json.Linq;
 
 namespace ArtifactsAPI.Models
 {
-    public class MoveResponse
+    public class MoveResponse : BaseResponse
     {
-        public StatusCode status_code { get; private set; }
-        public HttpResponseMessage response { get; private set; }
-
-        public Cooldown? cooldown { get; private set; }
         public Map? destination { get; private set; }
         public Character? character { get; private set; }
 
         public MoveResponse(HttpResponseMessage response)
         {
             var content = response.Content.ReadAsStringAsync().Result;
-            var json = JObject.Parse(content);
+            var json = JObject.Parse(content)["data"];
 
             status_code = (StatusCode)response.StatusCode;
             this.response = response;
@@ -24,6 +20,50 @@ namespace ArtifactsAPI.Models
             {
                 cooldown = new Cooldown(json["cooldown"]!);
                 destination = new Map(json["map"]!);
+                character = new Character(json["character"]!);
+            }
+        }
+    }
+
+    public class RestResponse : BaseResponse
+    {
+        public int hp_restored { get; private set; }
+        public Character? character { get; private set; }
+
+        public RestResponse(HttpResponseMessage response)
+        {
+            var content = response.Content.ReadAsStringAsync().Result;
+            var json = JObject.Parse(content)["data"];
+
+            status_code = (StatusCode)response.StatusCode;
+            this.response = response;
+
+            if (status_code == StatusCode.OK)
+            {
+                cooldown = new Cooldown(json["cooldown"]!);
+                hp_restored = (int)json["hp_restored"]!;
+                character = new Character(json["character"]!);
+            }
+        }
+    }
+
+    public class FightResponse : BaseResponse
+    {
+        public Fight? fight { get; private set; }
+        public Character? character { get; private set; }
+
+        public FightResponse(HttpResponseMessage response)
+        {
+            var content = response.Content.ReadAsStringAsync().Result;
+            var json = JObject.Parse(content)["data"];
+
+            status_code = (StatusCode)response.StatusCode;
+            this.response = response;
+
+            if (status_code == StatusCode.OK)
+            {
+                cooldown = new Cooldown(json["cooldown"]!);
+                fight = new Fight(json["fight"]!);
                 character = new Character(json["character"]!);
             }
         }
