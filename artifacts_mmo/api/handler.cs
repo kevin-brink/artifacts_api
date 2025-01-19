1536acd3c2af2f10d783c7891a21667ba93332ae
kevinbrink <kbrink170@gmail.com>
2025-01-19 12:16:53 -0600

    Remove unneeded imports

--- artifacts_mmo/api/handler.cs
+++ artifacts_mmo/api/handler.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace ArtifactsAPI
 {
