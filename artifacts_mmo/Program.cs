440bc3da9a68e40c3c5c47abfa3686a1a77a4f04
kevinbrink <kbrink170@gmail.com>
2025-01-20 07:16:58 -0600

    Organize map results

--- artifacts_mmo/Program.cs
+++ artifacts_mmo/Program.cs
@@ -11,12 +11,28 @@ string api_key =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImticmluazE3MEBnbWFpbC5jb20iLCJwYXNzd29yZF9jaGFuZ2VkIjoiIn0.j0jrus4jUQGyfQMwipWthHhIHfZrAeyAvav1eWhc0Q8";
 APIHandler api = new APIHandler(api_key, name);
 
-var client = new Client(api, name);
-await api.Actions.Move(0, 1);
-await client.GrindCombat(10);
+// var client = new Client(api, name);
+// await api.Actions.Move(0, 1);
+// await client.GrindCombat(10);
 
 // _ = await api.Actions.Move(0, 0);
 // var move = await api.Actions.Move(-1, 0);
 // var gather = await api.Actions.Gathering();
 
+var maps = await api.Maps.GetAllMapsTotal();
+var maps_with_content = maps.data.Where(x => x.content is not null).ToList();
+var content = new Dictionary<ContentType, Dictionary<string, List<Map>>>();
+foreach (var map in maps_with_content)
+{
+    var content_type = Enum.TryParse<ContentType>(map.content!.type, out var type)
+        ? type
+        : ContentType.any;
+    if (!content.ContainsKey(content_type))
+        content[content_type] = [];
+
+    if (!content[content_type].ContainsKey(map.content!.code))
+        content[content_type][map.content!.code] = new List<Map>();
+
+    content[content_type][map.content!.code].Add(map);
+}
 return 0;
